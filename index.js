e3f4b7d4c786ff36182575f28e789f3f2498e452
Max Binshtok <max@inteist.com>
2018-04-24 14:12:02 -0400

    initial commit with the code

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * Returns a hash code for a string - identical to Java's String.hashCode() method
+ *
+ * The hash code for a string object is computed as
+ *     s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
+ * The hash value of the empty string is zero
+ *
+ * @param {string} s a string
+ * @return {number} a hash code for the input string.
+ *
+ * Usage:
+ * const hasher = require ('java-hashCode')
+ * const hashCode = hasher.hasCode("inputString")
+ */
+exports.hashCode = function(s) {
+  var h = 0,
+    l = s.length,
+    i = 0;
+  if (l > 0) while (i < l) h = ((h << 5) - h + s.charCodeAt(i++)) | 0;
+  return h;
+};
